ec77ff24d743fb413242860b62aec3a6fba50898
Gaurav Chawla <chawlagauravv@gmail.com>
2022-05-06 20:01:20 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+       This is an about component
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis facere debitis autem reiciendis consequatur eligendi at laudantium totam soluta cupiditate accusamus, explicabo et natus sequi, ipsum nesciunt, qui dolores eum?</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+
+  return (
+    <div className="bg-dark text-light">
+      <footer className="text-center py-2" >
+      Copyright &copy; MyTodosList.com
+      </footer>
+        
+    </div>
+  )
+}
